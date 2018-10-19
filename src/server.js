14b7f00cbf74102a2106eafdf6675814adc792a6
kistea1 <aaron.e.kistenmacher@medtronic.com>
2018-10-19 13:43:51 -0600

    https redirect

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ SourceMapSupport.install();
 
 const app = express();
 
+app.use((req, res, next) => {
+  if (!req.secure && process.env.NODE_ENV !== 'development') {
+    return res.redirect(`https://${req.get('host')}${req.url}`);
+  }
+  return next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello Node');
 });
